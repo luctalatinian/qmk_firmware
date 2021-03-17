a59d0b1b354dc1f9d0423a6a7461166fdddfd947
Luc Talatinian <luc.talatinian@gmail.com>
2021-03-17 00:00:20 -0400

    reinstate led mode

--- keyboards/massdrop/ctrl/keymaps/luc/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/luc/keymap.c
@@ -3,6 +3,8 @@
 #include "raw_hid.h"
 #include "print.h"
 
+#define DRIVER_LED_UNDERGLOW_START 87
+
 enum ctrl_keycodes {
     BOOT = SAFE_RANGE, // Restart into bootloader after hold timeout
     LED_M0,            // LED mode 0 (all)
@@ -39,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, KC_SLCK, BOOT, \
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
-        TG(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
+        _______, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              KC_UP, \
         KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(2),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
     ),
@@ -75,43 +77,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint32_t TIMEOUT = 1000*60*15; // 15 minutes
 
 void matrix_init_user(void) {
+    // this doesn't shut the LEDs off, it just sets it to a static color,
+    // otherwise the default is the rotating RGB which I assume has some
+    // hardware cost (which is always thrown away by what we do in
+    // rgb_matrix_indicators_user anyway)
+    rgb_matrix_mode(RGB_MATRIX_NONE);
 };
 
 void matrix_scan_user(void) {
 };
 
-#define LED_ID0_FNKEYS   0b00000000000000000001111111111110
-#define LED_ID1_LETTERS  0b00001111111110000000111111111100
-#define LED_ID2_LETTERS  0b00000000000000000000000001111111
-#define LED_ID1_CAPSLOCK 0b00000000000001000000000000000000
-
-
 void raw_hid_receive(uint8_t* data, uint8_t length)
 {
-    dprintf("raw_hid_receive\n");
 }
 
-//uint32_t runtime_color_configval = 0;
-//uint32_t runtime_color_configpos = 0;
+// runtime color config
+uint8_t r = 0;
+uint8_t g = 0xff;
+uint8_t b = 0xff;
+
+uint32_t runtime_color_configval = 0;
+uint32_t runtime_color_configpos = 0;
 
 void handle_runtime_color_config(uint16_t keycode) {
-  //uint16_t v = keycode - CNF_0;
-  //runtime_color_configval |= v << (runtime_color_configpos++ * 4);
-  //if (runtime_color_configpos == 6) {
-  //    led_base.r = (runtime_color_configval >>  0) & 0xff;
-  //    led_base.g = (runtime_color_configval >>  8) & 0xff;
-  //    led_base.b = (runtime_color_configval >> 16) & 0xff;
+    uint16_t v = keycode - CNF_0;
+    runtime_color_configval |= v << (runtime_color_configpos++ * 4);
+    if (runtime_color_configpos == 6) {
+        r = (runtime_color_configval >>  0) & 0xff;
+        g = (runtime_color_configval >>  8) & 0xff;
+        b = (runtime_color_configval >> 16) & 0xff;
 
-  //    led_base.r = ((led_base.r&0x0f)<<4) | ((led_base.r&0xf0)>>4);
-  //    led_base.g = ((led_base.g&0x0f)<<4) | ((led_base.g&0xf0)>>4);
-  //    led_base.b = ((led_base.b&0x0f)<<4) | ((led_base.b&0xf0)>>4);
+        // human-readable version has the nibbles swapped from what they should be
+        r = ((r&0x0f)<<4) | ((r&0xf0)>>4);
+        g = ((g&0x0f)<<4) | ((g&0xf0)>>4);
+        b = ((b&0x0f)<<4) | ((b&0xf0)>>4);
 
-  //    led_instructions[0] = led_base;
-
-  //    runtime_color_configval = 0;
-  //    runtime_color_configpos = 0;
-  //}
+        runtime_color_configval = 0;
+        runtime_color_configpos = 0;
+    }
 }
+
+uint8_t led_mode = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
@@ -150,6 +156,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case LED_M1:
         case LED_M2:
         case LED_M3:
+            if (record->event.pressed)
+                led_mode = keycode - LED_M0;
             return false;
         case CNF_0:
         case CNF_1:
@@ -168,15 +176,42 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case CNF_E:
         case CNF_F:
             // never in one's life has case fallthrough been this relevant
-            if (record->event.pressed) {
+            if (record->event.pressed)
                 handle_runtime_color_config(keycode);
-            }
             return false;
         default:
             return true;
     }
 }
 
-void rgb_matrix_indicators_user(void) {
-    rgb_matrix_set_color_all(0, 0xff, 0xff);
+#define CONFIG r, g, b
+#define OFF 0, 0, 0
+
+#define LED_MODE_ALL       0
+#define LED_MODE_KEYS      1
+#define LED_MODE_UNDERGLOW 2
+#define LED_MODE_OFF       3
+
+void rgb_set_keys(uint8_t r, uint8_t g, uint8_t b)
+{
+    for (int i = 0; i < DRIVER_LED_UNDERGLOW_START; ++i)
+        rgb_matrix_set_color(i, r, g, b);
+}
+
+void rgb_set_underglow(uint8_t r, uint8_t g, uint8_t b)
+{
+    for (int i = DRIVER_LED_UNDERGLOW_START; i < DRIVER_LED_TOTAL; ++i)
+        rgb_matrix_set_color(i, r, g, b);
+}
+
+void rgb_matrix_indicators_user(void)
+{
+    if (led_mode == LED_MODE_ALL || led_mode == LED_MODE_KEYS)
+        rgb_set_keys(CONFIG);
+    else
+        rgb_set_keys(OFF);
+    if (led_mode == LED_MODE_ALL || led_mode == LED_MODE_UNDERGLOW)
+        rgb_set_underglow(CONFIG);
+    else
+        rgb_set_underglow(OFF);
 }
